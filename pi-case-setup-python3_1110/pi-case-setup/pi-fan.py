d94a76fa6379f3004d0431cab18892513f6abeb4
anonymouslojz <13428981281@163.com>
2023-07-24 17:05:35 +0800

    Update pi-fan.py

--- pi-case-setup-python3_1110/pi-case-setup/pi-fan.py
+++ pi-case-setup-python3_1110/pi-case-setup/pi-fan.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2021 INNOMAKER.
-# http://www.inno-maker.com/
+# Copyright (c) 2021 Pibiger.
+#
 #
 # Licensed under the GNU General Public License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,10 +15,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# tutorials url: http://www.inno-maker.com/
+# tutorials url: www.pibiger-tech.com
 
 __author__ = 'Jack'
-__license__  = 'Copyright (c) 2021 INNOMAKER'
+__license__  = 'Copyright (c) 2021 Pibiger'
 
 import time
 #import smbus
